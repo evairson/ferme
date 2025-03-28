b09159a2b9f6a3ddb4affdd36177e9495684c4e4
evairson <eva.herson@me.com>
2025-03-28 11:57:15 +0100

    pages + logo

--- script/sauvegarde.js
+++ script/sauvegarde.js
@@ -10,6 +10,32 @@ async function getCurrentFileSha({GITHUB_USERNAME, REPO, FILE_PATH, TOKEN}) {
     return data.sha;
   }
 
+export async function createToast({file}) {
+  const editor = new toastui.Editor({
+    el: document.querySelector('#editor'),
+    height: '500px',
+    initialEditType: 'wysiwyg',
+    previewStyle: 'vertical',
+    hooks: {
+        addImageBlobHook: (blob, callback) => {
+            image_list.push(blob);
+            console.log(blob);
+            callback(URL.createObjectURL(blob), blob.name);
+        }
+    },
+  });
+  fetch(file)
+      .then(response => response.text())
+      .then(html => {
+          const start = html.indexOf('<main class="prose">') ;
+          const end = html.indexOf('</main>');
+          const content = html.substring(start, end);
+          editor.setHTML(content);
+          console.log(content);
+      });
+  return editor;
+}
+
 export async function uploadImageToGitHub({ file, TOKEN }) {
     const GITHUB_USERNAME = "evairson";
     const REPO = "ferme";
@@ -53,6 +79,9 @@ export async function publishToGitHub({ file, html, TOKEN }) {
     <title>Document</title>
     </head>
        <header class="p-3 bg-green-950 text-white pl-20 py-5"> 
+        <a href="../index.html">
+            <img class="w-20" src="../images/logo.png" alt="Logo de la Ferme">
+        </a>
         <h1 class="text-3xl ml-20">La Ferme</h1>
         <i class="font-extralight ml-20 text-sm">4 chemin de Bignolas, La Marolle-en-Sologne</i>
     </header>
@@ -84,3 +113,37 @@ export async function publishToGitHub({ file, html, TOKEN }) {
     });
       return res.ok;
     }
+
+export async function uploadImagesToGitHub({ image_list, TOKEN }) {
+  for (let i = 0; i < image_list.length; i++) {
+    await uploadImageToGitHub({
+        file: image_list[i],
+        TOKEN : TOKEN})
+    .then((response) => {
+        const imageUrl = response.url;
+        const realName = imageUrl.split('/')[imageUrl.split('/').length - 1];
+        console.log("Image URL:", imageUrl);
+        console.log("Image name:", realName);
+        console.log("Image uploaded successfully:", imageUrl);
+
+        const img = document.querySelector(`img[alt="${image_list[i].name}"]`);
+            if (img) {
+            img.src = `https://evairson.github.io/ferme/images/importer/${realName}`;
+            console.log("Image updated in the editor:", img.src);
+            } else if (img === null) {
+                console.error("Image not found in the editor:", image_list[i].name);
+                throw new Error("Image not found in the editor");
+            } else {
+                console.error("Image not found in the editor:", image_list[i].name);
+                throw new Error("Image not found in the editor");
+            }
+            
+        
+    })
+    .catch((error) => {
+        console.error("Error uploading image:", error);
+        document.getElementById('status').textContent = "❌ Échec de la publication.";
+        throw error;
+    });
+    }
+}
